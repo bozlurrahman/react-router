8b518556e6d02b1bd03744e3065280ab2cb86121
Bozlur Rahman <bozlur.reve@gmail.com>
2019-06-13 13:12:59 +0600

    RecursiveRoute Example

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import NoMatchExample from './scripts/NoMatchExample';
+import RecursivePaths from './scripts/RecursivePaths';
 
-ReactDOM.render(<NoMatchExample />, document.getElementById('root'));
+ReactDOM.render(<RecursivePaths />, document.getElementById('root'));
