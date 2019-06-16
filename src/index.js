629494d86b534f2d0d611a945aa58afcd8c0e569
Bozlur Rahman <bozlur.reve@gmail.com>
2019-06-16 15:12:45 +0600

    modal galler example

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import RouteConfig from './scripts/RouteConfig';
+import ModalGallery from './scripts/ModalGallery';
 
-ReactDOM.render(<RouteConfig />, document.getElementById('root'));
+ReactDOM.render(<ModalGallery />, document.getElementById('root'));
